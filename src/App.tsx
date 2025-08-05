c084229012fba4a54097ec835f5f0dbfcf289878
HelloEnkio <hello@enkio.fr>
2025-08-05 10:18:26 +0200

    feat: implement cookie consent component

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Contact from "./pages/Contact";
 import Privacy from "./pages/Privacy";
 import Terms from "./pages/Terms";
 import NotFound from "./pages/NotFound";
+import CookieConsent from "@/components/CookieConsent";
 
 const queryClient = new QueryClient();
 
@@ -20,6 +21,7 @@ const App = () => (
       <Sonner />
       <ComparisonResultProvider>
         <BrowserRouter>
+          <CookieConsent />
           <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/contact" element={<Contact />} />
